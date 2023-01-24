ed38fcf1db78cc7457b740e92b6a3cfa81b1924c
romanproudly <roman@proudly.ru>
2023-01-24 13:56:42 +0300

    upd contacts

--- src/site/pages/ContactsPage.php
+++ src/site/pages/ContactsPage.php
@@ -2,6 +2,7 @@
 namespace Site\Pages;
 use Engine\Library\Page;
 use Engine\Library\Template;
+use Engine\Library\ListTemplate;
 use Engine\Library\Common;
 use Site\Components\BreadcrumbsComponent;
 use Site\Models\PageModel;
@@ -9,13 +10,28 @@ use Site\Models\PageModel;
 class ContactsPage extends Page {
   protected $_template;
 
+  const CODE = 'contacts';
+  const DIR = 'contacts';
+
   public function __construct() {
-    parent::__construct('contacts');
 
-    $name      = $this->code();
-    $directory = $name;
+    parent::__construct(self::CODE, self::DIR);
 
-    $this->_template = new Template($name, $directory);
+    // $name      = $this->code();
+    // $directory = $name;
+
+    //$this->_template = new Template($name, $directory);
+
+    $this->setPages([
+      'index' => ['template' => 'contacts'],
+    ]);
+
+    $this->setPartials([
+      'list' => [
+        'type' => 'list',
+        'template' => 'contacts__card'
+      ]
+    ]);
   }
 
   function index() {
@@ -31,6 +47,15 @@ class ContactsPage extends Page {
     $content = $pageModel->getContent($this->code());
     $content['PhoneLink'] = Common::GetTelLink($content['Phone']);
     $content['PhoneCommonLink'] = Common::GetTelLink($content['PhoneCommon']);
+
+    $storesModel = new \Site\Models\StoresModel($Database);
+    $stores = $storesModel->getAllItems();
+    foreach ($stores as $i => $store) {
+      $stores[$i]['TelLink'] = Common::GetTelLink($store['Tel']);
+      $stores[$i]['ImageWebp'] = Common::flGetWebpByImage($store['Image']);
+    }
+    $storesListTemplate = new ListTemplate('contacts__card', 'contacts/partial');
+    $storesListRendered = $storesListTemplate->parse($stores);
     
     $formTemplate = new Template('bl-feedback', $this->code());
     $formTemplate = $formTemplate->parse([
@@ -38,7 +63,8 @@ class ContactsPage extends Page {
         'Noise' => uniqid()
     ]);
 
-    return $this->_template->parse($content + [
+    return $this->getPage('index')->parse($content + [
+      'List' => $storesListRendered,
       'Form' => $formTemplate,
       'Breadcrumbs' => $breadcrumbsRendered
     ]);
