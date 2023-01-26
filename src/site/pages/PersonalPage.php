d3064c5f50101c25597edad3912bb817afa0c301
romanproudly <roman@proudly.ru>
2023-01-26 12:05:59 +0300

    fix

--- src/site/pages/PersonalPage.php
+++ src/site/pages/PersonalPage.php
@@ -75,7 +75,7 @@ class PersonalPage extends Page {
     $templateRendered = $templateData->parse([
       'Name' => $data['NameEn'],
       'Alt' => htmlspecialchars($data['Name'], ENT_QUOTES),
-      'Photo' => $data['Photo105_105'] ?: '/assets/images/_team_person.jpg',
+      'Photo' => $data['Photo105_105'] ? '<div class="photo"><img src="'.$data['Photo105_105'].'" alt="'.htmlspecialchars($data['Name'], ENT_QUOTES).'" title="'.htmlspecialchars($data['Name'], ENT_QUOTES).'"></div>' : '<div style="height:10vw; max-height:80px;"></div>',
       'Post' => $data['PostEn'],
       'Tel' => $data['TelEn'],
       'TelCode' => preg_replace('/[^\d\+]/', '', $data['Tel']),
